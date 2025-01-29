a2fdba3efd8f2c884e39e0904ee23ca6377d0bf2
ByteMaster69 <amitoj.mehta@gmail.com>
2025-01-29 15:15:13 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Oswald } from 'next/font/google';
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const oswald = Oswald({
+  subsets: ['latin'],
+  variable: '--font-oswald',
 });
 
 export const metadata = {
@@ -20,9 +17,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${oswald.className} antialiased`}
       >
+        <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
